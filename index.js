0828fe1337afea653d25986d1bcba86ea937ae30
staceyjf <staceyfanner@gmail.com>
2023-12-15 14:21:55 +1100

    additional JS for mobile

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const card = document.querySelector('.card');
 
-card.addEventListener('click', function() {
-    card.classList.toggle('open')
-});
+// Function to check if touch events are supported
+function isTouchDevice() {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+}
+
+// Function to handle click or touch event based on device type
+function handleCardInteraction() {
+    card.classList.toggle('open');
+
+    // Check if it's a touch device and toggle the show-right-card class accordingly
+    if (isTouchDevice()) {
+        card.classList.toggle('show-right-card');
+    }
+}
+
+// Add click or touch event listener based on device type
+if (isTouchDevice()) {
+    card.addEventListener('touchstart', handleCardInteraction);
+} else {
+    card.addEventListener('click', handleCardInteraction);
+}
