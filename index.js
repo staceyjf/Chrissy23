681e6ef522ecb5d3b46ca154ade826b57ad11a0a
staceyjf <staceyfanner@gmail.com>
2023-12-15 17:24:33 +1100

    tweak

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ function handleCardInteraction() {
     // Check if it's a touch device and toggle the show-right-card class accordingly
     if (isTouchDevice()) {
         card.classList.toggle('show-right-card');
+        card.classList.remove('open');
     } else {
         card.classList.toggle('open');
+        card.classList.remove('show-right-card');
     }
 }
 
 // Add click or touch event listener based on device type
-if (isTouchDevice()) {
+if (isTouchDevice()) {        card.classList.remove('open');
+
+
     card.addEventListener('touchstart', handleCardInteraction);
 } else {
     card.addEventListener('click', handleCardInteraction);
