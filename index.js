92c530f6804d2d7eaed3d36a74530a21bb4bcb0e
staceyjf <staceyfanner@gmail.com>
2023-12-18 11:17:12 +1100

    troubleshooting JS

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const card = document.querySelector('.card');
-const arrow = document.querySelector('#arrow');
+document.addEventListener('DOMContentLoaded', function () {
 
-// Function to check if touch events are supported
-function isTouchDevice() {
-    return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-}
+    const card = document.querySelector('.card');
+    const arrow = document.querySelector('#arrow');
 
-// Function to handle click or touch event based on device type
-function handleCardInteraction() {
-
-    // Check if it's a touch device and toggle the show-right-card class accordingly
-    if (isTouchDevice()) {
-        card.classList.toggle('show-right-card');
-        card.classList.remove('open');
-    } else {
-        card.classList.toggle('open');
-        card.classList.remove('show-right-card');
+    // Function to check if touch events are supported
+    function isTouchDevice() {
+        return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
     }
-}
 
-console.log(card, arrow);
+    // Function to handle click or touch event based on device type
+    function handleCardInteraction() {
 
-// Add click or touch event listener based on device type
-if (isTouchDevice()) {      
-    card.addEventListener('touchend', handleCardInteraction);
-} else {
-    card.addEventListener('click', handleCardInteraction);
-    arrow.addEventListener('click', handleCardInteraction);
-    console.log('yes i clciked')
-}
+        // Check if it's a touch device and toggle the show-right-card class accordingly
+        if (isTouchDevice()) {
+            card.classList.toggle('show-right-card');
+            card.classList.remove('open');
+        } else {
+            card.classList.toggle('open');
+            card.classList.remove('show-right-card');
+        }
+    }
+
+    console.log(card, arrow);
+
+    // Add click or touch event listener based on device type
+    if (isTouchDevice()) {      
+        card.addEventListener('touchend', handleCardInteraction);
+    } else {
+        card.addEventListener('click', handleCardInteraction);
+        arrow.addEventListener('click', handleCardInteraction);
+        console.log('event listner added')
+    }
+});
