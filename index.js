dc77a187a625e0b2ad499ba8a5591ae583f2031d
staceyjf <staceyfanner@gmail.com>
2023-12-18 11:23:25 +1100

    adjust for touchscreen latoptops

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     const card = document.querySelector('.card');
     const arrow = document.querySelector('#arrow');
 
-    // Function to check if touch events are supported
+    // Function to check if touch events are supported on screens smaller than 600 pixels
     function isTouchDevice() {
-        return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+        return window.innerWidth < 600 && ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0);
     }
 
     // Function to handle click or touch event based on device type
     function handleCardInteraction() {
-
         // Check if it's a touch device and toggle the show-right-card class accordingly
         if (isTouchDevice()) {
             card.classList.toggle('show-right-card');
@@ -24,11 +22,11 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(card, arrow);
 
     // Add click or touch event listener based on device type
-    if (isTouchDevice()) {      
+    if (isTouchDevice()) {
         card.addEventListener('touchend', handleCardInteraction);
     } else {
         card.addEventListener('click', handleCardInteraction);
         arrow.addEventListener('click', handleCardInteraction);
-        console.log('event listner added')
+        console.log('event listener added');
     }
 });
